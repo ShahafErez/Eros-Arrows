d680eb22ab6d495861c269fddc7f062a32cf5014
ShahafErez <Shahaf30@gmail.com>
2023-12-18 11:45:29 +0200

    accessing the server from the client. added Cors support.

--- API/Program.cs
+++ API/Program.cs
@@ -12,10 +12,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
