87c70779fa863bdd446cb66818570e89abeaf290
ShahafErez <Shahaf30@gmail.com>
2023-12-18 13:32:00 +0200

    added ssl to client

--- API/Program.cs
+++ API/Program.cs
@@ -18,7 +18,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 if (app.Environment.IsDevelopment())
 {
