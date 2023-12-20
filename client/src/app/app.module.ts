7a0874cb6d8f58721089485b8eb324f7f4ec8130
ShahafErez <Shahaf30@gmail.com>
2023-12-20 18:27:26 +0200

    home & register components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,9 +8,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
