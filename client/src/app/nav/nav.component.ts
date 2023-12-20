fc69e5c819b929d8d493e7861aaae1ac1794e8e3
ShahafErez <Shahaf30@gmail.com>
2023-12-20 17:48:05 +0200

    get logged in user from observable

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -8,33 +8,17 @@ import { AccountService } from './../_services/account.service';
 })
 export class NavComponent {
   model: any = {};
+  constructor(public accountService: AccountService) {}
 
-  loggedIn = false;
-
-  constructor(private accountService: AccountService) {}
-
-  ngOnInit(): void {
-    this.getCurrentUser();
-  }
-
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe({
-      next: (user) => (this.loggedIn = !!user),
-      error: (error) => console.log(error),
-    });
-  }
+  ngOnInit(): void {}
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: () => {
-        this.loggedIn = true;
-      },
       error: (error) => console.log(error),
     });
   }
 
   logout() {
     this.accountService.logout();
-    this.loggedIn = false;
   }
 }
