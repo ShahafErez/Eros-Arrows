60b6f95699ed662a8427370ecc20a989895828d1
ShahafErez <Shahaf30@gmail.com>
2023-12-23 16:23:02 +0200

    added fields to user entity & calculated age

---
+++ API/Entities/Photo.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Entities;
+
+public class Photo
+{
+    public int Id { get; set; }
+    public string Url { get; set; }
+    public string IsMain { get; set; }
+    public string PublicId { get; set; }
+
+}
