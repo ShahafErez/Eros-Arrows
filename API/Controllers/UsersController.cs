705870ed38da56bb7016620dc2fad049ce7e5f30
ShahafErez <90973511+ShahafErez@users.noreply.github.com>
2023-12-26 22:42:40 +0200

    Uploading new photo & Saving it on Cloudinary (#13)

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,5 @@
-﻿using System.Security.Claims;
-using API.DTOs;
+﻿using API.DTOs;
+using API.Entities;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -13,11 +13,13 @@ public class UsersController : BaseApiController
 {
     private readonly IUserRepository _userRepository;
     private readonly IMapper _mapper;
+    private readonly IPhotoService _photoService;
 
-    public UsersController(IUserRepository userRepository, IMapper mapper)
+    public UsersController(IUserRepository userRepository, IMapper mapper, IPhotoService photoService)
     {
         _userRepository = userRepository;
         _mapper = mapper;
+        _photoService = photoService;
     }
 
     [HttpGet]
@@ -35,12 +37,30 @@ public class UsersController : BaseApiController
     [HttpPut]
     public async Task<ActionResult> updateUser(MemberUpdateDto memberUpdateDto)
     {
-        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-        var user = await _userRepository.GetUserByUsernameAsync(username);
+        var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
 
         if (user == null) return NotFound();
         _mapper.Map(memberUpdateDto, user);
         if (await _userRepository.SaveAllAsync()) return NoContent();
         return BadRequest("Failed to update user");
+    }
+
+    [HttpPost("add-photo")]
+    public async Task<ActionResult<PhotoDto>> addPhoto(IFormFile file)
+    {
+        var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+        if (user == null) return NotFound();
+        var result = await _photoService.AddPhotoAsync(file);
+        if (result.Error != null) return BadRequest(result.Error.Message);
+        var photo = new Photo
+        {
+            Url = result.SecureUrl.AbsoluteUri,
+            PublicId = result.PublicId
+        };
+        // if it's the first photo by the user, we'll set it as mauin
+        if (user.Photos.Count == 0) photo.IsMain = true;
+        user.Photos.Add(photo);
+        if (await _userRepository.SaveAllAsync()) return _mapper.Map<PhotoDto>(photo);
+        return BadRequest("Problem adding photo");
     }
 }
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -3,6 +3,7 @@
 using API.Data;
 using API.Interfaces;
 using API.Services;
+using API.Helpers;
 
 namespace API.Extensions;
 
@@ -20,6 +21,8 @@ public static class ApplicationServiceExtensions
         services.AddScoped<ITokenService, TokenService>();
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+        services.AddScoped<IPhotoService, PhotoService>();
 
         return services;
     }
