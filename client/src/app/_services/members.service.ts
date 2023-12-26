a1ea0c1464d02e5d93c0ddd54af229cfb98bcac8
ShahafErez <Shahaf30@gmail.com>
2023-12-26 15:33:23 +0200

    save members in service

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,25 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, of } from 'rxjs';
+import { Member } from 'src/app/_models/member';
 import { environment } from 'src/environments/environment';
-import { Member } from '../_models/member';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MembersService {
   baseUrlUsers = environment.apiUrl + '/users';
+  members: Member[] = [];
 
   constructor(private http: HttpClient) {}
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrlUsers);
+    if (this.members.length > 0) return of(this.members);
+    return this.http.get<Member[]>(this.baseUrlUsers).pipe(
+      map((members) => {
+        this.members = members;
+        return members;
+      })
+    );
   }
 
   getMember(username: string) {
+    const member = this.members.find((x) => x.userName === username);
+    if (member) return of(member);
     return this.http.get<Member>(`${this.baseUrlUsers}/${username}`);
   }
 
   updateMember(member: Member) {
-    return this.http.put(this.baseUrlUsers, member);
+    return this.http.put(this.baseUrlUsers, member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = { ...this.members[index], ...member };
+      })
+    );
   }
 }
