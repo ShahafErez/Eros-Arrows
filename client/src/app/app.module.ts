12c2a271f697cf8145cadb99822b34f35dd0dc41
ShahafErez <Shahaf30@gmail.com>
2023-12-26 09:48:26 +0200

    added photos gallary

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -13,7 +13,6 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
-import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { NavComponent } from './nav/nav.component';
@@ -26,7 +25,6 @@ import { RegisterComponent } from './register/register.component';
     HomeComponent,
     RegisterComponent,
     MemberListComponent,
-    MemberDetailComponent,
     ListsComponent,
     MessagesComponent,
     NotFoundComponent,
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,15 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from './../../_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
+  standalone: true,
   templateUrl: './member-detail.component.html',
   styleUrls: ['./member-detail.component.css'],
+  imports: [CommonModule, TabsModule, GalleryModule],
 })
 export class MemberDetailComponent {
   member: Member | undefined;
+  images: GalleryItem[] = [];
 
   constructor(
     private membersService: MembersService,
@@ -24,7 +30,17 @@ export class MemberDetailComponent {
     var username = this.route.snapshot.paramMap.get('username');
     if (!username) return;
     this.membersService.getMember(username).subscribe({
-      next: (member) => (this.member = member),
+      next: (member) => {
+        this.member = member;
+        this.getImages();
+      },
     });
+  }
+
+  getImages() {
+    if (!this.member) return;
+    for (const photo of this.member?.photos) {
+      this.images.push(new ImageItem({ src: photo.url, thumb: photo.url }));
+    }
   }
 }
