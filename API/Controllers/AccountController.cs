b9251530553472f5125baac25ba489e02530fd6e
ShahafErez <Shahaf30@gmail.com>
2023-12-27 16:50:22 +0200

    add new fields to server

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,11 +14,13 @@ public class AccountController : BaseApiController
 {
     private readonly DataContext _context;
     private readonly ITokenService _tokenService;
+    private readonly IMapper _mapper;
 
-    public AccountController(DataContext context, ITokenService tokenService)
+    public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
     {
         _context = context;
         _tokenService = tokenService;
+        _mapper = mapper;
     }
 
     [HttpPost("register")]
@@ -26,21 +29,24 @@ public class AccountController : BaseApiController
         var username = registerDto.Username.ToLower();
         if (await UserExists(username)) return BadRequest("Username is taken");
 
+        var user = _mapper.Map<User>(registerDto);
+
         var password = registerDto.Password;
 
         using var hmac = new HMACSHA512();
-        var user = new User
-        {
-            UserName = username,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
-            PasswordSalt = hmac.Key
-        };
+
+        // TODO- move to mapper
+        user.UserName = username;
+        user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+        user.PasswordSalt = hmac.Key;
+
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
         return new UserDto
         {
             Username = user.UserName,
-            Token = _tokenService.CreateToken(user)
+            Token = _tokenService.CreateToken(user),
+            KnownAs = user.KnownAs
         };
     }
 
@@ -66,7 +72,8 @@ public class AccountController : BaseApiController
         {
             Username = user.UserName,
             Token = _tokenService.CreateToken(user),
-            PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+            PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+            KnownAs = user.KnownAs
         };
     }
 
